faa3ea4c37cf32db441d5b1c6c88e0556ad826d0
Amitesh Singh <amitesh.singh@corvallis.it>
2019-11-06 12:27:29 +0100

    [change] save data inside database

--- src/main/java/com/jwtspring/service/JwtUserDetailsService.java
+++ src/main/java/com/jwtspring/service/JwtUserDetailsService.java
@@ -1,20 +1,49 @@
 package com.jwtspring.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.jwtspring.dao.UserDao;
+import com.jwtspring.model.DAOUser;
+import com.jwtspring.model.UserDTO;
 
 @Service
 public class JwtUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UserDao userDao;
+
+	@Autowired
+	private PasswordEncoder bcryptEncoder;
 
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		if ("jwtsprint".equals(username)) {
-			return new User("jwtsprint", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
-					new Arrays<>());
-		} else {
+		
+		DAOUser user = (DAOUser) userDao.findByUsername(username);
+		
+		if (user == null) {
 			throw new UsernameNotFoundException("User not found with username: " + username);
 		}
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+				new ArrayList<>());
+		
+//		if ("jwtsprint".equals(username)) {
+//			return new User("jwtsprint", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+//					new Arrays<>());
+//		} else {
+//			throw new UsernameNotFoundException("User not found with username: " + username);
+//		}
 	}
+
+	public UserDao save(UserDTO user) {
+		DAOUser newUser = new DAOUser();
+		newUser.setUsername(user.getUsername());
+		newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+		return (UserDao) userDao.save(newUser);
+	}
+
 }
